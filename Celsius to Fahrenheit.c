f50a1ae7c21a7fc00f072d96d81674d22c43cd1f
Hasnain Ali <hasnainalirind@gmail.com>
2024-10-20 05:37:01 -0700

    Update Celsius to Fahrenheit.c

--- Celsius to Fahrenheit.c
+++ Celsius to Fahrenheit.c
@@ -6,14 +6,11 @@
 int main() {
     float celsius, fahrenheit;
     
-    // Reading temperature in Celsius
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
     
-    // Converting Celsius to Fahrenheit
     fahrenheit = (celsius * 9/5) + 32;
     
-    // Displaying the result
     printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
     
     return 0;
